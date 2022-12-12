36ef6bf5770df8939a36f75cf0c43e7cab67d170
8cott <scottrubin@gmail.com>
2022-12-12 17:29:25 -0500

    basic layout

---
+++ script.js
@@ -0,0 +1,26 @@
+// Functions
+// start game
+// generate question and answers
+// click an answer button
+// what happens if the answer is correct or incorrect
+// display results
+// log score +10 for each correct answer
+// move on to next question
+// if you get 3 incorrect you lose
+// game over if lose
+// if you reach score of 100 you win
+// clicking start over restarts game
+// 
+// 
+// need library of questions
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
