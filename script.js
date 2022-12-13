90a2a64339a54deac84e93e880d0865de3f50cfe
8cott <scottrubin@gmail.com>
2022-12-13 17:53:00 -0500

    populated questions and answers

--- script.js
+++ script.js
@@ -1,34 +1,33 @@
-
-
-// Functions
-// start game
-// generate question and answers
-// click an answer button
-// what happens if the answer is correct or incorrect
-// display results
-// log score +10 for each correct answer
-// move on to next question
-// if you get 3 incorrect you lose
-// game over if lose
-// if you reach score of 100 you win
-// clicking start over restarts game
-// 
-// 
-// need library of questions
-// 
+// load Open Trivia Library
 window.onload = getOpenTrivia
 
+// Async Function to fetch API data
 async function getOpenTrivia() {
-    let response = await fetch('https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple');
+    let response = await fetch('https://opentdb.com/api.php?amount=10&difficulty=easy&type=multiple');
     let data = await response.json()
     return data;
 }
- getOpenTrivia().then((data) => console.log(data.results));
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple
+
+// randomize array
+function shuffleAray(arr) {
+    for (let i = arr.length -1; i >= 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [arr[i], arr[j]] = [arr[j], arr[i]];
+    }  
+}
+
+ // get specific items from API array
+getOpenTrivia().then((data) => {
+    const results = data.results[0];
+    console.log(results);
+    document.getElementById('question').innerHTML = results.question;
+    document.getElementById('category').innerHTML = results.category;
+    document.getElementById('difficulty').innerHTML = results.difficulty;
+    const answers = [...results.incorrect_answers, results.correct_answer];
+    shuffleAray(answers);
+    for (let i = 0; i < 4; i++) { 
+        let index = i + 1
+        document.getElementById(`answer${index}`).innerHTML = answers[i]
+    }
+});
+
